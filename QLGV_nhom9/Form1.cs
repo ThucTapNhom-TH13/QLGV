0502ba60cf6cb8033b905ecbcf9213b49e1e71d3
phuonglinhth238 <phuonglinhth238@gmail.com>
2017-05-29 16:10:39 +0700

    Form1

--- QLGV_nhom9/Form1.cs
+++ QLGV_nhom9/Form1.cs
@@ -21,7 +21,36 @@ namespace QLGV_nhom9
 
         public  void ShowChild(Form parent, Form child)
         {
-         
+            if (parent == null) throw new ArgumentNullException("parent form is null");
+
+            //if (parent.HasChildren)
+            //{
+            //    foreach (var fi in parent.MdiChildren)
+            //    {
+            //        if (fi.GetType() != child.GetType()) continue;
+            //        fi.MdiParent = parent;
+            //        fi.ControlBox = false;
+            //        fi.StartPosition = FormStartPosition.CenterScreen;
+            //        fi.WindowState = FormWindowState.Normal;
+            //        fi.WindowState = FormWindowState.Maximized;
+            //        fi.Activate();
+            //        return;
+            //    }
+            //}
+            if (parent.HasChildren)
+            {
+                foreach (var fi in parent.MdiChildren)
+                {
+                    fi.Close();
+                }
+            }
+            child.MdiParent = parent;
+            parent.StartPosition = FormStartPosition.CenterScreen;
+            child.ControlBox = false;
+            child.WindowState = FormWindowState.Normal;
+            child.WindowState = FormWindowState.Maximized;
+            child.Show();
+            child.Activate();
         }
         
 
@@ -32,38 +61,38 @@ namespace QLGV_nhom9
 
         private void họcHàmToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            
+            this.ShowChild(this, new DanhSachHocHam());
         }
 
         private void họcVịToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            
+            this.ShowChild(this, new DanhSachHocVi());
         }
 
         
         private void chứcVụToolStripMenuItem_Click(object sender, EventArgs e)
         {
-           
+            this.ShowChild(this, new DanhSachChucVu());
         }
 
         private void mônHọcToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            
+            this.ShowChild(this, new DanhSachMonHoc());
         }
 
         private void toolGiaoVien_Click(object sender, EventArgs e)
         {
-            
+            this.ShowChild(this, new DanhSach());
         }
 
         private void toolInKhoa_Click(object sender, EventArgs e)
         {
-            
+            this.ShowChild(this, new DanhSachKhoa());
         }
 
         private void toolBoMon_Click(object sender, EventArgs e)
         {
-            
+            this.ShowChild(this, new DanhSachBoMon());
         }
 
         private void toolDanhMuc_Click(object sender, EventArgs e)
@@ -78,7 +107,7 @@ namespace QLGV_nhom9
 
         private void theoBộMônToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            
+            this.ShowChild(this, new thongketheobomon());
         }
 
         private void toolStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
@@ -88,32 +117,33 @@ namespace QLGV_nhom9
 
         private void theoChứcVụToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            
+            this.ShowChild(this, new thongketheochucvu());
         }
 
         private void theoKhoaToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            
+            this.ShowChild(this, new thongketheokhoa());
         }
 
         private void theoHọcHàmToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            
+            this.ShowChild(this, new thongketheohocham());
         }
 
         private void theoHọcVịToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-           
+            this.ShowChild(this, new thongketheohocvi());
         }
 
         private void toolTimKiem_Click(object sender, EventArgs e)
         {
-            
+            this.ShowChild(this, new TimKiem());
         }
 
         private void inẪnToolStripMenuItem_Click(object sender, EventArgs e)
         {
-           
+            //this.ShowChild(this, new InAn());
+            this.ShowChild(this, new FormLyLichGiaoVien());
         }
        
         
