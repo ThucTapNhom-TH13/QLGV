f3d1df4010a078a80e7cc52167e6d055890c5591
phuonglinhth238 <phuonglinhth238@gmail.com>
2017-05-29 16:32:53 +0700

    InAn

--- QLGV_nhom9/InAn.cs
+++ QLGV_nhom9/InAn.cs
@@ -19,24 +19,57 @@ namespace QLGV_nhom9
         }
         private void Load_HocVi()
         {
-          
+            List<SqlParameter> listPara = new List<SqlParameter>();
+            listPara.Add(new SqlParameter("@magiaovien", txtMaGV.Text.Trim()));
+            DataTable dt = a.GetDatastoreprocude("InAnHocVi", listPara);
+            dgvHocVi.DataSource = dt;
         }
         private void Load_HocHam()
         {
-           
+            List<SqlParameter> listPara = new List<SqlParameter>();
+            listPara.Add(new SqlParameter("@magiaovien", txtMaGV.Text.Trim()));
+            DataTable dt = a.GetDatastoreprocude("InAnHocHam", listPara);
+            dgvHocHam.DataSource = dt;
         }
         private void Load_MonHoc()
         {
-           
+            List<SqlParameter> listPara = new List<SqlParameter>();
+            listPara.Add(new SqlParameter("@magiaovien", txtMaGV.Text.Trim()));
+            DataTable dt = a.GetDatastoreprocude("InAnMonHoc", listPara);
+            dgvMonHoc.DataSource = dt;
         }
         private void btOK_Click(object sender, EventArgs e)
         {
-           
+            Load_HocVi();
+            Load_HocHam();
+            Load_MonHoc();
+            List<SqlParameter> listPara = new List<SqlParameter>();
+            listPara.Add(new SqlParameter("@magiaovien", txtMaGV.Text.Trim()));
+            DataTable dt = a.GetDatastoreprocude("InAn", listPara);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                if (dt.Rows[i]["MaGV"].ToString().Trim() == txtMaGV.Text.Trim())
+                {
+                    txtTenGiaoVien.Text = dt.Rows[i]["TenGiaoVien"].ToString();
+                    txtQueQuan.Text = dt.Rows[i]["quequan"].ToString();
+                    txtNgaySinh.Text = dt.Rows[i]["NgaySinh"].ToString();
+                    txtGioiTinh.Text = dt.Rows[i]["GioiTinh"].ToString();
+                    txtChucVu.Text = dt.Rows[i]["TenChucVu"].ToString();
+                    txtDanToc.Text = dt.Rows[i]["DanToc"].ToString();
+                    txtEmail.Text = dt.Rows[i]["Email"].ToString();
+                    txtSDT.Text = dt.Rows[i]["SoDienThoai"].ToString();
+                    txtKhoa.Text = dt.Rows[i]["TenKhoa"].ToString();
+                    txtBoMon.Text = dt.Rows[i]["TenBoMon"].ToString();
+                    break;
+                }
+            }
         }
 
         private void InAn_Load(object sender, EventArgs e)
         {
-           
+            Load_HocVi();
+            Load_HocHam();
+            Load_MonHoc();
         }
     }
 }
