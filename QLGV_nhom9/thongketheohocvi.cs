0491c5688e91e6a45093a2c1021fddb59bc36881
DESKTOP-MTOCE95\Linh <hualinh1012@gmail.com>
2017-06-04 16:40:09 +0700

    thong ke hoc ham + hoc vi + khoa

--- QLGV_nhom9/thongketheohocvi.cs
+++ QLGV_nhom9/thongketheohocvi.cs
@@ -19,16 +19,21 @@ namespace QLGV_nhom9
         }
         public void Load_TKHocVi()
         {
-          
+            List<SqlParameter> prm = new List<SqlParameter>();
+            prm.Add(new SqlParameter("mahocvi", cmbHocVi.SelectedValue.ToString().Trim()));
+            DataTable dt = a.GetData("select *from GiaoVien where  MaGV in (select MaGV from QuaTrinhDaoTao where MaHocVi=@mahocvi )", prm);
+            dgvTKHocVi.DataSource = dt;
         }
         private void thongketheohocvi_Load(object sender, EventArgs e)
         {
-          
+            cmbHocVi.DisplayMember = "TenHocVi";
+            cmbHocVi.ValueMember = "MaHocVi";
+            cmbHocVi.DataSource = a.GetData("select *from HocVi");
         }
 
         private void btnThongKe_Click(object sender, EventArgs e)
         {
-           
+            Load_TKHocVi();
         }
 
         private void btnHuy_Click(object sender, EventArgs e)
--- QLGV_nhom9/thongketheokhoa.cs
+++ QLGV_nhom9/thongketheokhoa.cs
@@ -12,6 +12,7 @@ namespace QLGV_nhom9
 {
     public partial class thongketheokhoa : Form
     {
+
         ChuoiKetNoi a = new ChuoiKetNoi();
         public thongketheokhoa()
         {
@@ -19,11 +20,16 @@ namespace QLGV_nhom9
         }
         public void Load_TKKhoa()
         {
-           
+            List<SqlParameter> prm = new List<SqlParameter>();
+            prm.Add(new SqlParameter("makhoa", cmbKhoa.SelectedValue.ToString().Trim()));
+            DataTable dt = a.GetData("select *from GiaoVien where MaKhoa=@makhoa", prm);
+            dgvTKKhoa.DataSource = dt;
         }
         private void thongketheokhoa_Load(object sender, EventArgs e)
         {
-          
+            cmbKhoa.DisplayMember = "TenKhoa";
+            cmbKhoa.ValueMember = "MaKhoa";
+            cmbKhoa.DataSource = a.GetData("select *from Khoa");
         }
 
         public int CountSogv()
@@ -37,7 +43,8 @@ namespace QLGV_nhom9
 
         private void btnThongKe_Click(object sender, EventArgs e)
         {
-           
+            Load_TKKhoa();
+            txtSoGV.Text = CountSogv().ToString();
 
         }
 
