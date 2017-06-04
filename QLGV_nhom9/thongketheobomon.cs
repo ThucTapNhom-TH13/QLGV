ed189296022a43397962937bed06ff786b375b2a
DESKTOP-MTOCE95\Linh <hualinh1012@gmail.com>
2017-06-04 16:39:03 +0700

    thong ke bo mon + chuc vu

--- QLGV_nhom9/thongketheobomon.cs
+++ QLGV_nhom9/thongketheobomon.cs
@@ -18,21 +18,35 @@ namespace QLGV_nhom9
             InitializeComponent();
         }
         public void Load_TKBoMon()
-        {        
-           
+        {
+            List<SqlParameter> prm = new List<SqlParameter>();
+            prm.Add(new SqlParameter("mabomon", cmbBoMon.SelectedValue.ToString().Trim()));
+            DataTable dt = a.GetData("select *from GiaoVien where MaBoMon=mabomon", prm);
+            dgvTKBoMon.DataSource = dt;
         }
         private void thongketheobomon_Load(object sender, EventArgs e)
         {
-           
+            //LoadDSBoMon();
+            LoadDSKhoa();
         }
         public void LoadDSKhoa()
         {
-          
+            //Load ds khoa
+
+            cmbKhoa.DisplayMember = "TenKhoa";
+            cmbKhoa.ValueMember = "MaKhoa";
+            cmbKhoa.DataSource = a.GetData("select *from Khoa");
+            LoadDSBoMon();
         }
 
         public void LoadDSBoMon()
         {
-           
+            //Load DS BoMon
+            cmbBoMon.DisplayMember = "TenBoMon";
+            cmbBoMon.ValueMember = "MaBoMon";
+            List<SqlParameter> prm = new List<SqlParameter>();
+            prm.Add(new SqlParameter("makhoa", cmbKhoa.SelectedValue.ToString().Trim()));
+            cmbBoMon.DataSource = a.GetData("select *from BoMon where MaKhoa=@makhoa", prm);
         }
         public int CountSogv()
         {
@@ -44,22 +58,25 @@ namespace QLGV_nhom9
 
         private void btnThongKe_Click(object sender, EventArgs e)
         {
-           
+            //LoadDSBoMon();
+            Load_TKBoMon();
+            // txtSoGV.Text = CountSogv().ToString();
         }
 
         private void btnHuy_Click(object sender, EventArgs e)
         {
-           
+            this.Close();
         }
 
         private void btnHuy_Click_1(object sender, EventArgs e)
         {
-           
+            this.Close();
         }
 
         private void btnThongKeBM_Click(object sender, EventArgs e)
         {
-            
+            Load_TKBoMon();
+            txtSoGV.Text = CountSogv().ToString();
         }
     }
 }
--- QLGV_nhom9/thongketheochucvu.cs
+++ QLGV_nhom9/thongketheochucvu.cs
@@ -19,21 +19,26 @@ namespace QLGV_nhom9
         }
         public void Load_TKChucVu()
         {
-           
+            List<SqlParameter> prm = new List<SqlParameter>();
+            prm.Add(new SqlParameter("machucvu", cmbChucVu.SelectedValue.ToString().Trim()));
+            DataTable dt = a.GetData("select *from GiaoVien where MaChucVu=@machucvu", prm);
+            dgvTKChucVu.DataSource = dt;
         }
         private void thongketheochucvu_Load(object sender, EventArgs e)
         {
-           
+            cmbChucVu.DisplayMember = "TenChucVu";
+            cmbChucVu.ValueMember = "MaChucVu";
+            cmbChucVu.DataSource = a.GetData("select *from ChucVu");
         }
 
         private void btnThongKe_Click(object sender, EventArgs e)
         {
-           
+            Load_TKChucVu();
         }
 
         private void btnHuy_Click(object sender, EventArgs e)
         {
-            
+            this.Close();
         }
     }
 }
